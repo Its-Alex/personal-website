4a5945f8cb2b01ae849f4dfe8251f5ebea52499c
Alex <xSkyZie@gmail.com>
2017-02-17 21:57:01 +0100

    Update to have somethings to show temporarily

---
+++ js/graph.js
@@ -0,0 +1,45 @@
+var doughnutOptions = {
+  segmentShowStroke : true,
+  segmentStrokeColor : "#333",
+  segmentStrokeWidth : 2,
+  percentageInnerCutout : 60,
+  animation : true,
+  animationSteps : 100,
+  animationEasing : "easeOutBounce",
+  animateRotate : true,
+  animateScale : true,
+}
+
+var doughnutData = [
+  {
+    label: "C",
+    value: 100,
+    color: "#CFC6C0",
+  },
+  {
+    label: "C++",
+    value : 70,
+    color : "#8F8985",
+  },
+  {
+    label: "JS",
+    value : 50,
+    color : "#5D5956",
+  },
+  {
+    label: "HTML",
+    value : 40,
+    color : "#43403E",
+  },
+  {
+    label: "CSS",
+    value : 40,
+    color : "#222",
+  }
+]
+
+// Get the context of the Doughnut Chart canvas element
+var ctx = document.getElementById("doughnutChart").getContext("2d");
+
+// Create the Doughnut Chart
+var mydoughnutChart = new Chart(ctx).Doughnut(doughnutData, doughnutOptions);
--- js/main.js
+++ js/main.js
@@ -3,27 +3,27 @@ particlesJS.load('particles-js', 'asset/particles.json');
 var vm = new Vue({
   el: '#particles-js',
   beforeCreate: function () {
-    axios.get('https://api.github.com/users/its-alex/repos', {})
-    .then((res) => {
-      var tmp = JSON.parse(res.request.response);
+    // axios.get('https://api.github.com/users/its-alex/repos', {})
+    // .then((res) => {
+    //   var tmp = JSON.parse(res.request.response);
 
-      console.log(tmp);
-      for (var count=0; count < tmp.length; count++){
-        var obj = {}
+    //   console.log(tmp);
+    //   for (var count=0; count < tmp.length; count++){
+    //     var obj = {}
 
-        obj.name = tmp[count].full_name.split('/')[1];
-        obj.link = tmp[count].html_url;
-        obj.description = tmp[count].description;
-        console.log(obj);
-        this.repos.push(obj);
-      }
-      console.log(this.repo);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+    //     obj.name = tmp[count].full_name.split('/')[1];
+    //     obj.link = tmp[count].html_url;
+    //     obj.description = tmp[count].description;
+    //     console.log(obj);
+    //     this.repos.push(obj);
+    //   }
+    //   console.log(this.repo);
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // })
   },
   data: {
-    repos: []
+    // repos: []
   }
 });
