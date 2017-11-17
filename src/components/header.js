534f46ce6448c6c4d9073afa5590abf7dec57bcf
ItsAlex <me@itsalex.fr>
2017-11-17 17:48:19 +0100

    Update for web

--- src/components/header.js
+++ src/components/header.js
@@ -8,8 +8,8 @@ class Header extends Component {
         <ul className='menu'>
           <a href=''><li>PROFILE</li></a>
           <a href=''><li>EXPERIENCES</li></a>
-          <a href=''><li>ETUDES</li></a>
-          <a href={`${process.env.PUBLIC_URL} + '/CV_Alexandre_FR.pdf'`}><li>CV</li></a>
+          <a href=''><li>EDUCATION</li></a>
+          <a href={`./CV_Alexandre_FR.pdf`} download='CV_fr.pdf' ><li>CV</li></a>
         </ul>
       </div>
     )
