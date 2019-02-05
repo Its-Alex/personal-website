b9a25fc94744f8c709c80dc1d758a4c17c0f94fb
ItsAlex <me@itsalex.fr>
2019-02-05 19:08:36 +0100

    feat: begin v2.0

---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    helloworld home
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'home',
+  components: {
+  }
+})
+</script>
