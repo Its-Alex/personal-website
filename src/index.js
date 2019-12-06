27f960ccac44f59967b529d718d73ae7e035c4b7
ItsAlex <me@itsalex.fr>
2019-12-07 00:56:00 +0100

    feat: add google analytics

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost'
 import { ApolloProvider } from '@apollo/react-hooks'
+import ReactGA from 'react-ga'
 
 import * as serviceWorker from './ServiceWorker'
 import GlobalTheme from './components/Theme'
 import Home from './pages/Home'
 import './i18n/I18n'
+
+ReactGA.initialize('UA-152708823-1')
 
 const httpLink = new HttpLink({ uri: 'https://api.github.com/graphql' })
 
