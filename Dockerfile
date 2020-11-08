9c3127821d58fe31bd5da5bb33ea11a3cffbfbe3
Its-Alex <me@itsalex.fr>
2020-11-08 22:57:47 +0100

    feat: add posthog

--- Dockerfile
+++ Dockerfile
@@ -5,11 +5,19 @@ ADD . /cv
 
 WORKDIR /cv
 
+ENV REACT_APP_CURRICULUM_POSTHOG_TOKEN=curriculum_posthog_token
+ENV REACT_APP_CURRICULUM_POSTHOG_URL=http://posthog.local
+
 RUN yarn install && yarn run build
 
 FROM nginx:1.15-alpine
 
 # Copy site file
 COPY --from=BUILDER /cv/build /usr/share/nginx/html
+COPY ./docker-entrypoint.sh /docker-entrypoint.sh
+RUN chmod u+x /docker-entrypoint.sh
 
-CMD ["nginx", "-g", "daemon off;"]
+ENV CURRICULUM_POSTHOG_TOKEN=curriculum_posthog_token
+ENV CURRICULUM_POSTHOG_URL=http://posthog.local
+
+CMD /docker-entrypoint.sh
---
+++ docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# shellcheck disable=SC2066
+set -e
+
+FILES_TO_REPLACE="$(find /usr/share/nginx/html -type f -iname '*.js')"
+
+for i in \
+    "curriculum_posthog_token;${CURRICULUM_POSTHOG_TOKEN}" \
+    "http://posthog.local;${CURRICULUM_POSTHOG_URL}"
+do
+    for filename in ${FILES_TO_REPLACE}; do
+        echo "Replacing $(echo "${i}" | cut -d';' -f1) by $(echo "${i}" | cut -d';' -f2) in ${filename}"
+        sed -i "s^$(echo "${i}" | cut -d';' -f1)^$(echo "${i}" | cut -d';' -f2)^g" "${filename}"
+    done
+done
+
+nginx -g "daemon off;"
