0985b13d845cac781afdf3d08faac454214ce6d6
ItsAlex <me@itsalex.fr>
2021-01-20 23:00:39 +0100

    chore: tidy deps

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from 'apollo-boost'
-import { ApolloProvider } from '@apollo/react-hooks'
 import ReactGA from 'react-ga'
 import * as Sentry from '@sentry/browser'
 import posthog from 'posthog-js'
@@ -25,29 +23,10 @@ if (process.env.REACT_APP_CURRICULUM_SENTRY_DSN) {
 ReactGA.initialize('UA-152708823-1')
 ReactGA.pageview('/')
 
-const httpLink = new HttpLink({ uri: 'https://api.github.com/graphql' })
-
-const authLink = new ApolloLink((operation, forward) => {
-  operation.setContext({
-    headers: {
-      authorization: 'Bearer 923c2ce575fe95918fe182a1ce0e60a8bea7c43f'
-    }
-  })
-
-  return forward(operation)
-})
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache()
-})
-
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <GlobalTheme>
-      <Home />
-    </GlobalTheme>
-  </ApolloProvider>
+  <GlobalTheme>
+    <Home />
+  </GlobalTheme>
   , document.getElementById('root'))
 
 serviceWorker.unregister()
