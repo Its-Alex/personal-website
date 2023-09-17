75194c224fcd8929ba949ab0d3b9c692928ed6ad
ItsAlex <me@itsalex.fr>
2023-09-17 23:31:35 +0200

    fix: allow use of queryLanguage to change language

--- src/lib/i18n/index.ts
+++ src/lib/i18n/index.ts
@@ -3,11 +3,22 @@ import { browser } from '$app/environment'
 import { init, register } from 'svelte-i18n'
 
 const defaultLocale = 'en'
+let queryLanguage: string | null = null
 
 register('en', async () => await import('./locales/en.json'))
 register('fr', async () => await import('./locales/fr.json'))
 
+if (browser) {
+  const searchParams = new URL(window.location.href).searchParams
+
+  if (searchParams?.has('queryLanguage')) {
+    queryLanguage = searchParams.get('queryLanguage')
+  }
+}
+
 init({
   fallbackLocale: defaultLocale,
-  initialLocale: browser ? window.navigator.language : defaultLocale
+  initialLocale: browser
+    ? queryLanguage ?? window.navigator.language
+    : defaultLocale
 })?.catch((err: Error) => { console.error(err) })
