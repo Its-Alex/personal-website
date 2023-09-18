d5768100b9443241c07ed70cf86c1a93e9cb18a6
ItsAlex <me@itsalex.fr>
2023-09-18 23:02:39 +0200

    --wip-- [skip ci]

--- src/routes/tech-radar/+page.svelte
+++ src/routes/tech-radar/+page.svelte
@@ -11,38 +11,103 @@
     anglePercentage: number;
   }
 
+  interface RadarConfig {
+    width: number;
+    height: number;
+    radius: number;
+    rings: string[];
+    circularSectors: string[];
+  }
+
   onMount(() => {
     // Les données du Tech Radar (exemple)
     const data: TechnologyData[] = [
-      { category: "Adopter", name: "Svelte", circularSector: "Quadrant1", radiusPercentage: 45, anglePercentage: 95 },
-      { category: "Adopter", name: "D3.js", circularSector: "Quadrant2", radiusPercentage: 20, anglePercentage: 20 },
-      { category: "Évaluer", name: "GraphQL", circularSector: "Quadrant3", radiusPercentage: 30, anglePercentage: 30 },
-      { category: "Évaluer", name: "TypeScript", circularSector: "Quadrant4", radiusPercentage: 40, anglePercentage: 40 },
-      { category: "Essayer", name: "WebAssembly", circularSector: "Quadrant1", radiusPercentage: 50, anglePercentage: 50 },
-      { category: "Essayer", name: "Tailwind CSS", circularSector: "Quadrant2", radiusPercentage: 10, anglePercentage: 25 },
-      { category: "Éviter", name: "jQuery", circularSector: "Quadrant3", radiusPercentage: 70, anglePercentage: 70 },
-      { category: "Éviter", name: "AngularJS", circularSector: "Quadrant4", radiusPercentage: 80, anglePercentage: 85 },
+      {
+        category: "Adopter",
+        name: "Svelte",
+        circularSector: "Quadrant1",
+        radiusPercentage: 45,
+        anglePercentage: 95,
+      },
+      {
+        category: "Adopter",
+        name: "D3.js",
+        circularSector: "Quadrant1",
+        radiusPercentage: 20,
+        anglePercentage: 20,
+      },
+      {
+        category: "Évaluer",
+        name: "GraphQL",
+        circularSector: "Quadrant1",
+        radiusPercentage: 30,
+        anglePercentage: 30,
+      },
+      {
+        category: "Évaluer",
+        name: "TypeScript",
+        circularSector: "Quadrant1",
+        radiusPercentage: 40,
+        anglePercentage: 40,
+      },
+      {
+        category: "Essayer",
+        name: "WebAssembly",
+        circularSector: "Quadrant1",
+        radiusPercentage: 50,
+        anglePercentage: 50,
+      },
+      {
+        category: "Essayer",
+        name: "Tailwind CSS",
+        circularSector: "Quadrant1",
+        radiusPercentage: 10,
+        anglePercentage: 25,
+      },
+      {
+        category: "Éviter",
+        name: "jQuery",
+        circularSector: "Quadrant1",
+        radiusPercentage: 70,
+        anglePercentage: 70,
+      },
+      {
+        category: "Éviter",
+        name: "AngularJS",
+        circularSector: "Quadrant1",
+        radiusPercentage: 50,
+        anglePercentage: 50,
+      },
     ];
 
-    // Configuration du Tech Radar
-    const radarConfig = {
-      width: 600,
-      height: 600,
-      radius: 250,
+    const radarConfig: RadarConfig = {
+      width: 500,
+      height: 0,
+      radius: 0,
       rings: ["Adopter", "Évaluer", "Essayer", "Éviter"],
-      circularSectors: ["Quadrant1", "Quadrant2", "Quadrant3", "Quadrant4", "Quadrant5", "Quadrant6"],
+      circularSectors: ["Quadrant1", "Quadrant2", "Quadrant3", "Quadrant4"],
     };
+    radarConfig.height = radarConfig.width;
+    radarConfig.radius = radarConfig.width / 2 - 10;
 
-    // Fonction pour convertir les données en coordonnées polaires
     const convertToPolar = (d: TechnologyData) => {
       const ringIndex = radarConfig.rings.indexOf(d.category);
-      const circularSectorIndex = radarConfig.circularSectors.indexOf(d.circularSector);
-      const circularSectorsSize = (2 * Math.PI) / radarConfig.circularSectors.length;
+      const circularSectorIndex = radarConfig.circularSectors.indexOf(
+        d.circularSector
+      );
+      const circularSectorsSize =
+        (2 * Math.PI) / radarConfig.circularSectors.length;
 
-      const angle = circularSectorsSize * d.anglePercentage / 100 + circularSectorIndex * circularSectorsSize;
-      const currentRingRadius = ((radarConfig.radius * (ringIndex + 1)) / radarConfig.rings.length);
-      const previousRingRadius = ((radarConfig.radius * (ringIndex)) / radarConfig.rings.length);
-      const radius = previousRingRadius + (currentRingRadius - previousRingRadius) * d.radiusPercentage / 100;
+      const angle =
+        (circularSectorsSize * d.anglePercentage) / 100 +
+        circularSectorIndex * circularSectorsSize;
+      const currentRingRadius =
+        (radarConfig.radius * (ringIndex + 1)) / radarConfig.rings.length;
+      const previousRingRadius =
+        (radarConfig.radius * ringIndex) / radarConfig.rings.length;
+      const radius =
+        previousRingRadius +
+        ((currentRingRadius - previousRingRadius) * d.radiusPercentage) / 100;
       const centerX = radarConfig.width / 2;
       const centerY = radarConfig.height / 2;
 
@@ -52,24 +117,23 @@
       };
     };
 
-    // Créer le SVG
     const svg = d3
-      .select<SVGSVGElement, TechnologyData[]>("#radar")
+      .select<HTMLDivElement, TechnologyData[]>("#radar")
       .append("svg")
-      .attr("width", radarConfig.width)
-      .attr("height", radarConfig.height);
+      .attr("viewBox", `0 0 ${radarConfig.width} ${radarConfig.height}`);
 
-    // Dessiner les quadrants 
     radarConfig.circularSectors.forEach((circularSector, index) => {
       const quadrant = index; // Identifie le quadrant
-      const circularSectorsSize = (2 * Math.PI) / radarConfig.circularSectors.length;
-      const offsetAngle = quadrant * circularSectorsSize; // Ajoute un décalage pour chaque quadrant
+      const circularSectorsSize =
+        (2 * Math.PI) / radarConfig.circularSectors.length;
+      const offsetAngle = quadrant * circularSectorsSize;
 
       svg
         .append("path")
         .attr(
           "d",
-          d3.arc<SVGPathElement, TechnologyData[]>()
+          d3
+            .arc<SVGPathElement, TechnologyData[]>()
             .innerRadius(0)
             .outerRadius(radarConfig.radius)
             .startAngle(offsetAngle)
@@ -78,12 +142,11 @@
         .attr(
           "transform",
           `translate(${radarConfig.width / 2},${radarConfig.height / 2})`
-        ) // Centre le quadrant au même centre que les cercles
+        )
         .attr("stroke", "gray")
         .attr("fill", "none");
     });
 
-    // Dessiner les cercles de chaque anneau
     radarConfig.rings.forEach((ring, index) => {
       svg
         .append("circle")
@@ -97,31 +160,23 @@
         .attr("fill", "none");
     });
 
-    // Créer le scaleOrdinal avec les types appropriés
     const colors = d3
       .scaleOrdinal<string>()
       .domain(radarConfig.rings.map(String))
       .range(["#7fc97f", "#beaed4", "#fdc086", "#ffff99"]);
 
-    // Dessiner les points pour chaque technologie
     svg
       .selectAll("circle.tech")
       .data(data)
       .enter()
       .append("circle")
       .attr("class", "tech")
-      .attr(
-        "cx",
-        (d: TechnologyData) =>
-          convertToPolar(d).x
-      )
-      .attr(
-        "cy",
-        (d: TechnologyData) =>
-          convertToPolar(d).y
-      )
+      .attr("cx", (d: TechnologyData) => convertToPolar(d).x)
+      .attr("cy", (d: TechnologyData) => convertToPolar(d).y)
       .attr("r", 5)
-      .attr("fill", (d: TechnologyData) => colors(String(radarConfig.rings.indexOf(d.category))))
+      .attr("fill", (d: TechnologyData) =>
+        colors(String(radarConfig.rings.indexOf(d.category)))
+      )
       .append("title")
       .text((d: TechnologyData) => `${d.name} - ${d.category}`);
   });
@@ -130,12 +185,25 @@
 <div id="radar"></div>
 
 <style>
-  :global(svg) {
+  #radar {
+    display: block;
+    margin: 0 auto;
+    width: 50%;
+    padding-bottom: 50%;
+  }
+
+  #radar:after {
+    content: "";
+    display: block;
+    padding-bottom: 100%;
+  }
+
+  :global(#radar svg) {
     display: block;
     margin: 0 auto;
   }
 
-  :global(circle.tech) {
+  :global(#radar circle.tech) {
     cursor: pointer;
   }
 </style>
