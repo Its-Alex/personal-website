2165f765ccba1cc684d107b8cf1821b54c5f953c
ItsAlex <me@itsalex.fr>
2024-11-17 15:59:37 +0100

    feat: add smiley to say hello

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
   import LeftBar from '$lib/frontpage/LeftBar.svelte'
   import Description from '$lib/frontpage/Description.svelte'
+
+  let isAnimating: boolean = false
+  const onMouseOver = (): void => {
+    if (!isAnimating) {
+      isAnimating = true
+    }
+  }
+  const onAnimationEnd = (): void => {
+    isAnimating = false
+  }
 </script>
 
 <div class="custom-container bg-gray-100">
   <LeftBar />
   <div class="custom-content">
-    <div>
+    <div class="flex flex-col flex-wrap">
+      <div class="flex items-center justify-center">
+        <div
+          role="none"
+          class="inline-block cursor-pointer text-6xl {isAnimating
+            ? 'animate-[wave_2s_ease-in-out]'
+            : ''}"
+          on:mouseover={onMouseOver}
+          on:focus={onMouseOver}
+          on:animationend={onAnimationEnd}
+        >
+          👋
+        </div>
+      </div>
       <Description />
     </div>
   </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ import typography from '@tailwindcss/typography'
 const config = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    extend: {
+      keyframes: {
+        wave: {
+          '0%, 100%': { transform: 'rotate(0deg)' },
+          '50%': { transform: 'rotate(45deg)' }
+        }
+      }
+    }
   },
   plugins: [typography]
 }
