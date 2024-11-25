a5a097544f1e821da134df3b5419d0da33a11788
ItsAlex <me@itsalex.fr>
2024-11-25 18:32:03 +0100

    feat: add notes page to share page from my PKM

---
+++ src/routes/notes/[...slug]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  /* eslint-disable svelte/no-at-html-tags */
+  export let data
+</script>
+
+<div class="mx-auto w-1/2">
+  <navbar>
+    <h1 class="my-10 text-2xl font-bold">{data?.meta?.title}</h1>
+  </navbar>
+  <div class="prose max-w-none">
+    {@html typeof data?.content === 'string' ? data.content : ''}
+  </div>
+</div>
