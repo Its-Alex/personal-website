421a170f19d827e8bab3c9c1a6daede8f54f1d1c
ItsAlex <me@itsalex.fr>
2025-04-04 22:45:18 +0200

    fix: remove header if there is no value in note

--- src/routes/notes/[...slug]/+page.svelte
+++ src/routes/notes/[...slug]/+page.svelte
@@ -14,7 +14,9 @@
 
 <div class="mx-auto w-1/2">
   <navbar>
-    <h1 class="my-10 text-2xl font-bold">{data?.meta?.title}</h1>
+    {#if data?.meta?.title !== ''}
+      <h1 class="my-10 text-2xl font-bold">{data?.meta?.title}</h1>
+    {/if}
   </navbar>
   <div class="prose max-w-none">
     {@html typeof data?.content === 'string' ? data.content : ''}
