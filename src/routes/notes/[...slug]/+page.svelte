b5d7a4c74dc96ee078b27a2fe9088124a3cc6fa8
ItsAlex <me@itsalex.fr>
2025-04-27 19:16:49 +0200

    feat: improve note styling

--- src/routes/notes/[...slug]/+page.svelte
+++ src/routes/notes/[...slug]/+page.svelte
@@ -12,13 +12,13 @@
   const { data }: Props = $props()
 </script>
 
-<div class="mx-auto w-1/2 p-4">
+<div class="prose mx-auto w-1/2 max-w-none p-2">
   <navbar>
     {#if data?.meta?.title}
-      <h1 class="pb-4 text-4xl font-bold">{data?.meta?.title}</h1>
+      <h1 class="text-5xl font-bold">{data?.meta?.title}</h1>
     {/if}
   </navbar>
-  <article class="prose bg-red max-w-none pb-4">
+  <article>
     {@html typeof data?.content === 'string' ? data.content : ''}
   </article>
   <footer>
