866109e3b48299b03a7ea04cd51c0ff4ff210f20
ItsAlex <me@itsalex.fr>
2025-04-28 19:11:19 +0200

    fix: lato was not working

--- src/routes/notes/[...slug]/+page.svelte
+++ src/routes/notes/[...slug]/+page.svelte
@@ -12,13 +12,13 @@
   const { data }: Props = $props()
 </script>
 
-<div class="prose mx-auto w-1/2 max-w-none p-2">
+<div class="mx-auto w-1/2 py-3">
   <navbar>
     {#if data?.meta?.title}
       <h1 class="text-5xl font-bold">{data?.meta?.title}</h1>
     {/if}
   </navbar>
-  <article>
+  <article class="prose max-w-none py-2">
     {@html typeof data?.content === 'string' ? data.content : ''}
   </article>
   <footer>
